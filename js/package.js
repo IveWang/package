1123a348f03a3f42d7270781e8d9bf91b696e58b
IveWang <Ivework@outlook.com>
2015-05-12 06:56:32 +0800

    update move

--- js/package.js
+++ js/package.js
@@ -9,13 +9,37 @@
 		af6 = document.createElement('div'),
 		af7 = document.createElement('div');
 	af0.setAttribute('class','animtCorner-lt');
+    af0.style.height = '1px';
+    af0.style.position = 'absolute';
+    af0.style.backgroundColor = 'red';
 	af1.setAttribute('class','animtCorner-lt-l');
+    af0.style.width = '1px';
+    af0.style.position = 'absolute';
+    af0.style.backgroundColor = 'red';
 	af2.setAttribute('class','animtCorner-rt');
+    af0.style.height = '1px';
+    af0.style.position = 'absolute';
+    af0.style.backgroundColor = 'red';
 	af3.setAttribute('class','animtCorner-rt-l');
+    af0.style.width = '1px';
+    af0.style.position = 'absolute';
+    af0.style.backgroundColor = 'red';
 	af4.setAttribute('class','animtCorner-rb');
+    af0.style.height = '1px';
+    af0.style.position = 'absolute';
+    af0.style.backgroundColor = 'red';
 	af5.setAttribute('class','animtCorner-rb-l');
+    af0.style.width = '1px';
+    af0.style.position = 'absolute';
+    af0.style.backgroundColor = 'red';
 	af6.setAttribute('class','animtCorner-lb');
+    af0.style.height = '1px';
+    af0.style.position = 'absolute';
+    af0.style.backgroundColor = 'red';
 	af7.setAttribute('class','animtCorner-lb-l');
+    af0.style.width = '1px';
+    af0.style.position = 'absolute';
+    af0.style.backgroundColor = 'red';
 	for (var i = 0; i < o.length; i++) {
 		var t = o[i].querySelector('input[type="text"]');
 		if (t) {
@@ -28,6 +52,7 @@
 				this.parentNode.appendChild(af5);
 				this.parentNode.appendChild(af6);
 				this.parentNode.appendChild(af7);
+                change(af0,);
 			}
 			// t.onblur=function() {
 			// 	this.parentNode.removeChild(af0);
@@ -41,21 +66,21 @@
 			// }
 		};
 	};
-	var func = {
-		easeOut: function(initPos, targetPos, currentCount, count) {
-            var b = initPos, c = targetPos - initPos, t = currentCount, d = count;
-            return -c * (t /= d) * (t - 2) + b;
-        }
-	};
-	function change (count) {
+	function change (element, width, height, top, right, bottom, left, count) {
 		var currentCount = 0;
-		count = Math.abs(count) || 1;
+		count = Math.abs(count) || 3;
 		var flag = setInterval(function(){
 			if ( currentCount > count ) {
 				clearInterval(flag)
 			} else{
-				currentCount++;
+				currentCount+=0.06;
+                if (width) element.style.Width = width - currentCount + 'px';
+                if (height) element.style.Height = height - currentCount + 'px';
+                if (top) element.style.top = top - currentCount + 'px';
+                if (left) element.style.left= left - currentCount + 'px';
+                if (bottom) element.style.bottom= bottom - currentCount + 'px';
+                if (right) element.style.right= right - currentCount + 'px';
 			};
-		},5)
+		},2)
 	}
 }(window)
